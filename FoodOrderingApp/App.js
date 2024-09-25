84cdc86aa521fe06407f7e6fac1041d4d0af6cc8
akhila <akhilavarghese174@gmail.com>
2024-09-25 22:31:54 +0530

    Added body contents resto cards

--- FoodOrderingApp/App.js
+++ FoodOrderingApp/App.js
@@ -21,10 +21,39 @@ const Header = () => {
         </div>
     )
 };
+
+
+// restaurant cards
+const RestCard = () => {
+    return (
+        <div className="res-card">
+            <img  className="res-logo" alt = "res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/r5mfli8wbcxukj0ehbit"/>
+            <h4>Hotel Mithila</h4>
+            <h6>North indian, Chinese</h6>
+            <h6>4.5 stars</h6>
+            <h6>35-40 mins</h6>
+        </div>
+    );
+};
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                <h3>Search</h3>
+            </div>
+            <div className="res-container">
+                <RestCard />
+                <RestCard />
+            </div>
+        </div>
+    );
+};
 const AppLayout = () => {
     return (
         <div id="app">
             <Header />  
+            <Body />
         </div>
     )
 };
